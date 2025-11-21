4a82442269507643468bbcc22496726f93e6fa2d
mysStack <64385837+mysStack@users.noreply.github.com>
2025-11-21 01:10:53 +0000

    删除

--- bankend/routers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-路由模块
-"""
-
-from .skopeo_router import skopeo_router
-
-__all__ = ["skopeo_router"]
--- bankend/services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-服务模块
-"""
-
-from .image_sync_service import ImageSyncService
-
-__all__ = ["ImageSyncService"]
